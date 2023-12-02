2c3eeb05044ce859f20fcd2231575d80898549a9
Aurelio Borges <aurelioaborges@gmail.com>
2023-12-02 16:05:47 -0300

    AC-09: setup basic page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,8 @@ import products from "@/services/api/productsApi";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/* <ProductsList products={products} /> */}
       <h2>All Products ({products.length})</h2>
-
-      <ProductsList products={products} />
     </main>
   );
 }
