b60acd25374a54d397266eefc3b65be7fca81d80
Aurelio Borges <aurelioaborges@gmail.com>
2023-12-04 14:40:54 -0300

    AC-09 update: MyProducts component

--- src/components/MyProducts.tsx
+++ src/components/MyProducts.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import Thumb from "@/assets/headphones.jpeg";
 
 export default function MyProducts({ myProducts }: any) {
   const [displayRows, setDisplayRows] = useState(4);
@@ -18,30 +17,33 @@ export default function MyProducts({ myProducts }: any) {
 
   return (
     <>
-      <div className="grid grid-cols-4 items-center gap-10">
+      <div className="grid grid-cols-4 items-center gap-10 pb-10">
         {myProducts.slice(0, displayRows * 4).map((product: any) => (
           <div
             className="flex flex-col justify-between gap-4 pb-5"
             key={product.id}
           >
-            <Link href={""}>
-              <Image src={Thumb} alt={"Headphones photo"} />
+            <Link key={product.id} href={`/products/${product.id}`}>
+              <Image
+                className="aspect-square"
+                width={300}
+                height={200}
+                src={product.url}
+                alt={"Random image for random product"}
+                priority
+              />
             </Link>
             <p className="text-left text-lg font-medium">{product.name}</p>
-
-            {/* <p className="text-center text-base font-medium sm:text-left sm:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
-            {product.description}
-          </p> */}
 
             <p className="text-left text-lg">R$ {product.price}</p>
           </div>
         ))}
       </div>
 
-      <div className="flex justify-center gap-10">
+      <div className="flex justify-evenly">
         {myProducts.length > 0 && displayRows * 4 < myProducts.length && (
           <button
-            className="rounded bg-gray-800 px-4 py-2 text-sm text-white transition-all duration-300 ease-linear hover:bg-gray-600 dark:bg-gray-400 dark:text-gray-950 dark:hover:bg-gray-200 dark:hover:text-black sm:text-base md:w-60 md:px-6 md:py-3 md:text-lg lg:text-xl xl:w-72 xl:px-8 xl:py-4 xl:text-2xl 2xl:w-96 2xl:px-10 2xl:py-5 2xl:text-3xl"
+            className="flex w-72 items-center justify-center gap-3 rounded-xl bg-stone-800 py-3 text-base text-stone-100 transition-all duration-300 ease-linear hover:bg-stone-600 hover:text-stone-50"
             onClick={loadMore}
           >
             Load more
@@ -49,7 +51,7 @@ export default function MyProducts({ myProducts }: any) {
         )}
 
         <button
-          className="rounded bg-gray-800 px-4 py-2 text-sm text-white transition-all duration-300 ease-linear hover:bg-gray-600 dark:bg-gray-400 dark:text-gray-950 dark:hover:bg-gray-200 dark:hover:text-black sm:text-base md:w-60 md:px-6 md:py-3 md:text-lg lg:text-xl xl:w-72 xl:px-8 xl:py-4 xl:text-2xl 2xl:w-96 2xl:px-10 2xl:py-5 2xl:text-3xl"
+          className="flex w-72 items-center justify-center gap-3 rounded-xl bg-stone-300 py-3 text-base text-stone-900 transition-all duration-300 ease-linear hover:bg-stone-200"
           onClick={scrollToTop}
         >
           Back to Top
