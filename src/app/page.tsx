46d619642680e7f424f51d2521b1c1106704a635
Aurelio Borges <aurelioaborges@gmail.com>
2023-12-04 14:42:36 -0300

    AC-04: implement products API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,29 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import Loading from "@/components/Loading";
 import MyProducts from "@/components/MyProducts";
-import myProducts from "@/services/api/api";
 
 export default function Home() {
+  const [myProducts, setMyProducts] = useState(null);
+
+  useEffect(() => {
+    fetch("/data.json")
+      .then((response) => response.json())
+      .then((myProducts: any) => setMyProducts(myProducts));
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col justify-between p-10">
-      <h2 className="mb-10 text-left text-2xl">
-        All Products: ({myProducts.length})
-      </h2>
+      {!myProducts ? (
+        <Loading />
+      ) : (
+        <>
+          <h2 className="mb-10 text-left text-2xl">Todos os produtos</h2>
 
-      <MyProducts myProducts={myProducts} />
+          <MyProducts myProducts={myProducts} />
+        </>
+      )}
     </main>
   );
 }
