198e3b1a689358cb71bada444ee4be5e2fcb8310
Aurelio Borges <aurelioaborges@gmail.com>
2023-12-06 16:32:04 -0300

    AC-09 fix: meta description and global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,14 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
+import { ShopProvider } from "@/services/providers/ShopProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Acme Inc",
   description:
-    "Acme Inc is a company that sells generic products and values the quality of its solutions and always being one step ahead of the market in terms of the use of technology and design of its applications.",
+    "A Acme Inc é uma empresa que comercializa produtos genéricos e preza pela qualidade de suas soluções e está sempre um passo à frente do mercado no uso de tecnologia e design de suas aplicações.",
 };
 
 export default function RootLayout({
@@ -19,8 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar />
-        {children}
+        <ShopProvider>
+          <NavBar />
+          {children}
+        </ShopProvider>
       </body>
     </html>
   );
